57acac1c76da41034d2ae0b4bb13c7da154a0206
Joel Yek <joevo2@gmail.com>
2018-04-20 23:13:53 +0800

    Style the content

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,16 +11,29 @@ export default class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Spending',
   };
-  
+
   render() {
+    const date = new Date()
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <Text>Hello World</Text>
+          <View style={styles.card}>
+            <View style={styles.cardDate}>
+              <Text>{date.toLocaleDateString()}</Text>
+            </View>
+            <View style={styles.cardRow}>
+              <Text>Food</Text>
+              <Text>RM 8</Text>
+            </View>
+            <View style={styles.cardRow}>
+              <Text>Grocery</Text>
+              <Text>RM 2</Text>
+            </View>
+          </View>
         </ScrollView>
         <View style={styles.tabBarInfoContainer}>
-          <Text style={{fontWeight: 'bold'}}>Total</Text>
-          <Text style={{fontWeight: 'bold'}}>RM 10</Text>
+          <Text style={{ fontWeight: 'bold' }}>Total</Text>
+          <Text style={{ fontWeight: 'bold' }}>RM 10</Text>
         </View>
       </View>
     );
@@ -33,7 +46,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   contentContainer: {
-    paddingTop: 30,
+    // paddingTop: 30,
+  },
+  card: {
+    borderWidth: 0.5,
+    borderColor: '#d6d7da'
+  },
+  cardDate: {
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    backgroundColor: '#f5f5f5'
+  },
+  cardRow: {
+    padding: 10,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
   },
   tabBarInfoContainer: {
     position: 'absolute',
